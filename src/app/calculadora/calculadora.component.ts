c298833bd04878131ef855d8f49143a850f5a392
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-03-12 17:20:19 -0300

    Adiciona operadores e data binding

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.css'
+  styleUrl: './calculadora.component.css',
 })
 export class CalculadoraComponent {
+  result: number = 0;
+  num1: number = 0;
+  num2: number = 0;
+  operator: string = '';
 
+  calculate(): void {
+    switch (this.operator) {
+      case '+':
+        this.result = this.num1 + this.num2;
+        break;
+      case '-':
+        this.result = this.num1 - this.num2;
+        break;
+      case '*':
+        this.result = this.num1 * this.num2;
+        break;
+      case '/':
+        this.result = this.num1 / this.num2;
+        break;
+      case '^':
+        this.result = Math.pow(this.num1, this.num2);
+        break;
+      case '%':
+        this.result = this.num1 % this.num2;
+        break;
+      default:
+        this.result = 0;
+    }
+  }
 }
